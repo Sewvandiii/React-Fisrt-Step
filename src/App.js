ae0fb65497b15c626991d6d94c76ac2a12f86065
sewvandipw.98@gmail.com <sewvandi.promodya@gmail.com>
2020-03-21 13:56:56 +0530

    Create a functional component - Greet.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Greet} from './Components/Greet'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+    <Greet />
     </div>
   );
 }
 
 export default App;
+//include greet component in App component
+//can use any name of greet in importing 
+//im going to change the greet into MyComponent
---
+++ src/Components/Greet.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+// function Greet(){
+//     return <h1>Hello Sewvandi</h1>
+// }
+
+//This will not display in the browser since we dont export it to App component
+//ES6 arrow function sysntax
+
+export const Greet = () => <h1>Hello Sewvandi</h1>
+//this is called a "Named export"
+
+// export default Greet
