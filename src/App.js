3e1a189a17c147fab9c76acb18a36fb3462d4b84
sewvandipw.98@gmail.com <sewvandi.promodya@gmail.com>
2020-03-21 19:57:27 +0530

    Change the title

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import {Greet} from './Components/Greet'
 import Welcome from './Components/Welcome'
+import Hello from './Components/Hello'
 
 function App() {
   return (
---
+++ src/Components/Hello.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Hello = () => {
+    return(
+        <div>
+            <h1>Hello Wendy</h1>
+        </div>
+    )
+} 
+//this is the JSX version of Hello Components
+
+export default Hello
