3dfa777a29be95173e328350ae63504cbda29938
Sewvandiii <sewvandipw.98@gmail.com>
2020-03-22 13:19:50 +0530

    Create a component with JSX and without JSX

--- src/Components/Hello.js
+++ src/Components/Hello.js
@@ -1,12 +1,15 @@
 import React from 'react'
 
 const Hello = () => {
-    return(
-        <div>
-            <h1>Hello Sewvandi</h1>
-        </div>
-    )
+    // return(
+    //     <div className = 'dummyclass'>
+    //         <h1>Hello Sewvandiii</h1>
+    //     </div>
+    // )
+    return React.createElement('div',{id:'hello',className: 'dummyclass'},React.createElement('h1',null,'hello wendi'))
 } 
-//this is the JSX version of Hello Components
+
+//this is the JSX version of Hello Components 
+//without using JSX //3 parameters
 
 export default Hello
