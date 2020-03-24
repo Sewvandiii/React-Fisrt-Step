18bf4699090c0d47db135e48689050e7da34f9c2
Sewvandiii <sewvandipw.98@gmail.com>
2020-03-24 16:04:56 +0530

    Index as key attribute

--- src/Components/NameList.js
+++ src/Components/NameList.js
@@ -3,22 +3,24 @@ import Person from './Person'
 
 function NameList() {
 
-    const persons=[{
-        id: 1,
-        name :'bruno',
-        age :34,
-        skill : 'Angular'
-    },
-    {
-        id: 2,
-        name :'aria',
-        age :34,
-        skill : 'Java'
-    }
-]
-    const personList = persons.map(person => <Person key = {person.id} person = {person} />)
+    const name =['bruno', 'aria', 'mia', 'bruno ']
+    const NameList = name.map((name,index) => <h2 key = {index}>{index} {name}</h2>)
+//     const persons=[{
+//         id: 1,
+//         name :'bruno',
+//         age :34,
+//         skill : 'Angular'
+//     },
+//     {
+//         id: 2,
+//         name :'aria',
+//         age :34,
+//         skill : 'Java'
+//     }
+// ]
+    // const personList = persons.map(person => <Person key = {person.id} person = {person} />)
     //passing person as a props to Person component
-    return <div>{personList}</div> 
+    return <div>{NameList}</div> 
 
             // <h2>{names[0]}</h2>
             // <h2>{names[1]}</h2>
